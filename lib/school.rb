106d81a398158f8a36de12837112a3b2af63d268
Ben Blackman <bblkmn5@gmail.com>
2017-10-13 02:27:58 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -18,7 +18,13 @@ def grade(student_grade)
 end
 
 def sort
-  
+    sorted_students = {}
+    roster.each do |grade, students|
+      sorted_students[grade] = students.sorted_students
+    end
+    sorted_students
+  end
+  end
 
 
 
